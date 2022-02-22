33c2ed9ce0e019bd45cc7eed2d9016758df9dc13
alex belz <oleksandr.belzetskyi.work@gmail.com>
2022-02-22 16:14:45 +0200

    fixed key problems

--- solution2/src/components/Main/Main.js
+++ solution2/src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import CouncilorsItem from '../CouncilorsItem/CouncilorsItem';
 import TextField from '@material-ui/core/TextField';
 import styled from 'styled-components';
@@ -10,145 +10,197 @@ import AffairItem from '../AffairItem/AffairItem';
 import { uuid } from 'uuidv4';
 
 const Root = styled.div`
-    width: 1400px;
-    padding: 1rem;
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    justify-content: space-around;
+  width: 1400px;
+  padding: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  justify-content: space-around;
 `;
 
 const CouncilorsWrapper = styled.div`
-    flex: 0.6;
+  flex: 0.6;
 `;
 
 const AffairsWrapper = styled.div`
-    flex: 0.3
+  flex: 0.3;
 `;
 
 const ButtonContainer = styled.div`
-    margin: 1rem;
+  margin: 1rem;
 `;
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 `;
 
 const OptionContainer = styled.div`
-    padding: 1rem;
-    border: 1px solid lightrey;
-    border-radius: 5px;
-    background-color: white;
+  padding: 1rem;
+  border: 1px solid lightrey;
+  border-radius: 5px;
+  background-color: white;
 `;
 
 const InputWrapper = styled.div`
-    padding: 0.5rem;
+  padding: 0.5rem;
 `;
 
 const DataWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    table-gap: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  table-gap: none;
 `;
 
 const Councilors = () => {
+  const [councilors, setCouncilors] = useState([]);
+  const [affairs, setAffairs] = useState([]);
 
-    const [councilors, setCouncilors] = useState([]);
-    const [affairs, setAffairs] = useState([]);
+  const [nameInputValue, setNameInputValue] = useState('');
+  const [idInputValue, setIdinputValue] = useState('');
 
-    const [nameInputValue, setNameInputValue] = useState('');
-    const [idInputValue, setIdinputValue] = useState('');
+  const [affairsInputValue, setAffairsInputValue] = useState('');
 
-    const[ affairsInputValue, setAffairsInputValue ] = useState('');
+  const [nameFilteredData, setNameFilteredData] = useState([]);
+  const [idFilteredData, setIdFilteredData] = useState([]);
 
-    const [nameFilteredData, setNameFilteredData] = useState([]);
-    const [idFilteredData, setIdFilteredData] = useState([]);
+  const [affairsFiltered, setAffairsFiltered] = useState([]);
 
-    const [affairsFiltered, setAffairsFiltered] = useState([]);
+  useEffect(() => {
+    httpConsumer.getCouncilors().then((data) => {
+      setCouncilors([...sorter.sortByName(data)]);
+    });
+    httpConsumer.getAffairs().then((data) => {
+      setAffairs(data);
+    });
+  }, []);
 
-    useEffect(() => {
-        httpConsumer.getCouncilors().then(data => {
-            setCouncilors([...sorter.sortByName(data)]);
-        })
-        httpConsumer.getAffairs().then(data => {
-            setAffairs(data);
-        })
-    }, [])
+  const handleNameSort = () => {
+    setCouncilors([...sorter.sortByName(councilors)]);
+  };
 
-    const handleNameSort = () => {
-        setCouncilors( [...sorter.sortByName(councilors)]);
-    }
+  const handleIDSort = () => {
+    setCouncilors([...sorter.sortByID(councilors)]);
+  };
 
-    const handleIDSort = () => {
-        setCouncilors( [...sorter.sortByID(councilors)]);
-    }
+  const handleNameInputValue = (event) => {
+    setNameInputValue(event.target.value);
+    setNameFilteredData([
+      ...filter.filterByName(councilors, event.target.value),
+    ]);
+  };
 
-    const handleNameInputValue = (event) => {
-        setNameInputValue(event.target.value);
-        setNameFilteredData([...filter.filterByName(councilors, event.target.value)]);
-    }
+  const handleIdInputValue = (event) => {
+    setIdinputValue(event.target.value);
+    setIdFilteredData([...filter.filterById(councilors, event.target.value)]);
+  };
 
-    const handleIdInputValue = (event) => {
-        setIdinputValue(event.target.value);
-        setIdFilteredData([...filter.filterById(councilors, event.target.value)]);
-    }
+  const handleAffairsInput = (event) => {
+    setAffairsInputValue(event.target.value);
+    setAffairsFiltered([...filter.filterByDate(affairs, event.target.value)]);
+  };
 
-    const handleAffairsInput = (event) => {
-        setAffairsInputValue(event.target.value);
-        setAffairsFiltered([...filter.filterByDate(affairs, event.target.value )])
-    }
-
-    return (
-        <Root>
-            <CouncilorsWrapper>
-            <OptionContainer>
-                <ButtonWrapper>
-                    <ButtonContainer>
-                        <Button color='primary' variant="contained" onClick={handleNameSort}>Sort by Name</Button>
-                    </ButtonContainer>
-                    <ButtonContainer>
-                        <Button color='primary' variant="contained" onClick={handleIDSort}>Sort by ID</Button>
-                    </ButtonContainer>
-                </ButtonWrapper>
-                <ButtonWrapper>
-                    <InputWrapper>
-                    <TextField value={nameInputValue} onChange={handleNameInputValue} label="Find by name:"/>
-                    </InputWrapper>
-                    <InputWrapper>
-                        <TextField value={idInputValue} onChange={handleIdInputValue} label="Find by Id:" />
-                    </InputWrapper>
-                </ButtonWrapper>
-            </OptionContainer>
-            <div>Councilors</div>
-            {nameInputValue && nameFilteredData.map(el => {
-                return <CouncilorsItem id={el.id} name={el.name} code={el.code}/>
+  return (
+    <Root>
+      <CouncilorsWrapper>
+        <OptionContainer>
+          <ButtonWrapper>
+            <ButtonContainer>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={handleNameSort}
+              >
+                Sort by Name
+              </Button>
+            </ButtonContainer>
+            <ButtonContainer>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={handleIDSort}
+              >
+                Sort by ID
+              </Button>
+            </ButtonContainer>
+          </ButtonWrapper>
+          <ButtonWrapper>
+            <InputWrapper>
+              <TextField
+                value={nameInputValue}
+                onChange={handleNameInputValue}
+                label="Find by name:"
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <TextField
+                value={idInputValue}
+                onChange={handleIdInputValue}
+                label="Find by Id:"
+              />
+            </InputWrapper>
+          </ButtonWrapper>
+        </OptionContainer>
+        <div>Councilors</div>
+        {nameInputValue &&
+          nameFilteredData.map((el) => {
+            return (
+              <CouncilorsItem
+                key={uuid()}
+                id={el.id}
+                name={el.name}
+                code={el.code}
+              />
+            );
+          })}
+        {idInputValue &&
+          idFilteredData.map((el) => {
+            return (
+              <CouncilorsItem
+                key={uuid()}
+                id={el.id}
+                name={el.name}
+                code={el.code}
+              />
+            );
+          })}
+        <DataWrapper>
+          {!nameInputValue &&
+            !idInputValue &&
+            councilors.map((el) => {
+              return (
+                <CouncilorsItem
+                  key={uuid()}
+                  id={el.id}
+                  name={el.name}
+                  code={el.code}
+                />
+              );
             })}
-            {idInputValue && idFilteredData.map(el => {
-                return <CouncilorsItem id={el.id} name={el.name} code={el.code} />
-            })}
-            <DataWrapper>
-            {(!nameInputValue && !idInputValue) && councilors.map(el => {
-                return <CouncilorsItem id={el.id} name={el.name} code={el.code}/>
-            })}
-            </DataWrapper> 
-            </CouncilorsWrapper>
-            <AffairsWrapper>
-            <OptionContainer>
-                <InputWrapper>
-                    <TextField value={affairsInputValue} onChange={handleAffairsInput} type='date' />
-                </InputWrapper>
-            </OptionContainer>
-                <div>Affairs</div>
-                {affairsInputValue && affairsFiltered.map(el => {
-                    return <AffairItem id={uuid()} date={el.updated} />
-                }) }
-                {!affairsInputValue && affairs.map(el => {
-                    return <AffairItem key={uuid()} id={el.id} date={el.updated}/>
-                })}
-            </AffairsWrapper>
-        </Root>
-    )
-}
+        </DataWrapper>
+      </CouncilorsWrapper>
+      <AffairsWrapper>
+        <OptionContainer>
+          <InputWrapper>
+            <TextField
+              value={affairsInputValue}
+              onChange={handleAffairsInput}
+              type="date"
+            />
+          </InputWrapper>
+        </OptionContainer>
+        <div>Affairs</div>
+        {affairsInputValue &&
+          affairsFiltered.map((el) => {
+            return <AffairItem key={uuid()} id={uuid()} date={el.updated} />;
+          })}
+        {!affairsInputValue &&
+          affairs.map((el) => {
+            return <AffairItem key={uuid()} id={el.id} date={el.updated} />;
+          })}
+      </AffairsWrapper>
+    </Root>
+  );
+};
 
 export default Councilors;
